bd007f1dc9f17230cbeadb8194a90e9d760f8780
Wesley Carvalho <crafael.wesley@gmail.com>
2022-06-15 12:02:33 -0300

    DEPLOY

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,22 @@ module.exports = {
 
 
     extend: {
-      spacing: {
+      backgroundImage: {
+        'hero-bg-home': "url('../src/images/bg-hero-home.png')",
+        'hero-bg-produtos': "url('../src/images/bg-hero-produtos.png')",
+        'hero-bg-quem-somos': "url('../src/images/bg-hero-quem-somos.png')",
+        'hero-bg-business-scan': "url('../src/images/bg-hero-business-scan.png')",
+        'hero-bg-soluções': "url('../src/images/bg-hero-solucoes.png')",
+        'hero-bg-revendas': "url('../src/images/bg-hero-revendas.png')",
+        'hero-bg-contato': "url('../src/images/bg-hero-contato.png')",
+        'bg-tarja-quemsomos': "url('../src/images/tarjaquemsomos.png')",
+        'bg-tarja-produtos' : "url('../src/images/tarjaprodutos.png')",
+        'bg-tarja-home' : "url('../src/images/tarjahome.png')",
+        'bg-form' : "url('../src/images/bg-form.png')",
+        'GED' : "url('../src/images/BusinessScan/GED.png')"
+      },
+
+      height: {
         '100': '6.25rem',
         '200': '12.5rem',
         '300': '18.75rem',
@@ -34,6 +49,12 @@ module.exports = {
         '900': '56.25rem',
         '1000': '62.5rem',
 
+      },
+
+
+      backgroundColor: {
+        'gradiente-blue' : "transparent linear-gradient(90deg, #0F5197 0%, #0088FF 100%) 0% 0% no-repeat padding-box",
+        'grayColor' : '#F8F8FA',
       },
 
 
