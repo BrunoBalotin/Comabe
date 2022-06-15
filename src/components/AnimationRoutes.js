a13ee9d50bcb3623db2b561093a3992d0ba10454
Wesley Carvalho <crafael.wesley@gmail.com>
2022-06-15 11:21:24 -0300

    deploy

--- src/components/AnimationRoutes.js
+++ src/components/AnimationRoutes.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
-import Contato from '../pages/Contato'
+
 import Home from '../pages/Home'
-import Produtos from '../pages/Produtos'
+
 
 import { AnimatePresence } from 'framer-motion'
-import QuemSomos from '../pages/QuemSomos'
-import BusinessScan from '../pages/BusinessScan'
-import Solucoes from '../pages/Solucoes'
-import TrabalheConosco from '../pages/TrabalheConosco'
-import Revendas from '../pages/Revendas'
+
 
 
 function AnimationRoutes() {
@@ -20,13 +16,7 @@ function AnimationRoutes() {
       <AnimatePresence>
     <Routes location={location} key={location.pathname}>
         <Route path='/' element={<Home/>} />
-        <Route path='/Quem-Somos' element={<QuemSomos/>} />
-        <Route path='/Business-Scan' element={<BusinessScan/>} />
-        <Route path='/Produtos' element={<Produtos/>} />
-        <Route path='/Solucoes' element={<Solucoes/>} />
-        <Route path='/Contato' element={<Contato/>} />
-        <Route path='/Trabalhe-Conosco' element={<TrabalheConosco/>} />
-        <Route path='/Revendas' element={<Revendas/>} />
+
 
      
         
