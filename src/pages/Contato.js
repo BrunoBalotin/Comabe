d7b5ceebb98a99ce89ae520cd06afb5c0648fa5b
wesleyeagles <84718917+wesleyeagles@users.noreply.github.com>
2022-06-16 16:08:53 -0300

    Add files via upload

--- src/pages/Contato.js
+++ src/pages/Contato.js
@@ -48,66 +48,66 @@ const locations = [
 const Contato = () => {
   return (
     <motion.div initial={{opacity: 0}} animate={{opacity: 1, transition: {duration: 0.8} }} exit={{opacity: 0, transition: {duration: 0.1} }}>
-    <div className="bg-hero-bg-contato w-full h-[700px] flex items-center relative">
+    <div className="bg-hero-bg-contato w-full py-36 flex items-center relative">
       <div className='container mx-auto relative'>
-        <div className='flex justify-between'>
-          <div className='w-[50%]'>
-          <span className='flex w-max bg-[#0088FF] py-2 px-5 text-white text-[18px] font-medium rounded-3xl'>ENTRE EM CONTATO</span>
-          <h1 className='text-[48px] text-white font-medium mt-[20px]'>Temos um equipe especializada para atendê-lo da melhor maneira possível.</h1>
-          <h3 className='text-[24px] text-white font-medium mt-[40px] mb-[30px]'>Nossos canais de atendimento</h3>
+        <div className='flex flex-col lg:flex-row justify-between'>
+          <div className='w-full lg:w-[50%]'>
+          <span className='flex w-max bg-[#0088FF] py-2 px-5 text-white text-sm font-medium rounded-3xl'>ENTRE EM CONTATO</span>
+          <h1 className='text-2xl lg:text-4xl text-white font-medium mt-[20px]'>Temos um equipe especializada para atendê-lo da melhor maneira possível.</h1>
+          <h3 className='text-xl text-white font-medium mt-[40px] mb-[30px]'>Nossos canais de atendimento</h3>
           <div className='flex flex-col gap-3'>
-          <span className='font-light text-white text-[18px] flex items-center gap-3'><Whatsapp />+55 54 2621.2300</span>
-          <span className='font-light text-white text-[18px] flex items-center gap-3'><Phone />+55 54 2621.2300</span>
-          <span className='font-light text-white text-[18px] flex items-center gap-3'><Mail />comabe@comabe.com.br</span>
+          <span className='font-light text-white text-sm flex items-center gap-3'><Whatsapp />+55 54 2621.2300</span>
+          <span className='font-light text-white text-sm flex items-center gap-3'><Phone />+55 54 2621.2300</span>
+          <span className='font-light text-white text-sm flex items-center gap-3'><Mail />comabe@comabe.com.br</span>
           </div>
           </div>
 
-          <div className='absolute right-0'>
+          <div className='lg:absolute right-0 mt-10 lg:mt-0'>
             <ReactForm fullWidth="w-full"/>
           </div>
 
         </div>
       </div>
-      <Mouse className='absolute -bottom-7 left-1/2 -translate-x-1/2' />
+      <Mouse className='absolute -bottom-11 lg:-bottom-9 2xl:-bottom-7 left-1/2 -translate-x-1/2' />
     </div>
 
     <div className='flex items-center h-[500px]'>
       <div className='container mx-auto'>
-        <div className='flex justify-between items-center'>
+        <div className='flex flex-col lg:flex-row justify-between lg:items-center'>
           <div>
-            <h2 className='text-[42px] text-[#343434] font-medium w-[70%]'>Estamos prontos para te atender</h2>
+            <h2 className='text-3xl text-[#343434] font-medium w-[70%]'>Estamos prontos para te atender</h2>
           </div>
 
-          <div className='w-[60%]'>
-            <p className='text-[18px] text-[#575757] font-light'>A equipe da Comabe está à disposição para atender você, seja para falar mais sobre como otimizar seu ambiente de trabalho, conversar sobre o seu plano atual, saber mais sobre a empresa ou dar a sua opinião sobre os nossos serviços. Para isso, é só preencher o formulário que em breve retornaremos.</p>
+          <div className='lg:w-[60%] mt-10 lg:mt-0'>
+            <p className='text-sm text-[#575757] font-light'>A equipe da Comabe está à disposição para atender você, seja para falar mais sobre como otimizar seu ambiente de trabalho, conversar sobre o seu plano atual, saber mais sobre a empresa ou dar a sua opinião sobre os nossos serviços. Para isso, é só preencher o formulário que em breve retornaremos.</p>
           </div>
         </div>
       </div>
 
     </div>
 
-    <div className='flex items-center h-[900px] bg-[#F8F8FA]'>
+    <div className='flex items-center py-28 bg-[#F8F8FA]'>
       <div className='container mx-auto'>
-        <div className='flex justify-between items-center'>
+        <div className='flex flex-col lg:flex-row gap-5 lg:gap-0 justify-between items-center'>
           <div>
-            <h2 className='text-[42px] text-[#343434] font-medium'>Visite-nos</h2>
+            <h2 className='text-3xl text-[#343434] font-medium'>Visite-nos</h2>
           </div>
 
           <div>
-          <span className='text-white bg-[#FF8800] flex items-center p-2 pl-[20px] pr-[20px] rounded-full text-[18px] font-medium gap-[10px]'><Pin /> Aonde estamos fisicamente</span>
+          <span className='text-white bg-[#FF8800] flex items-center p-2 pl-[20px] pr-[20px] rounded-full text-sm font-medium gap-[10px]'><Pin /> Aonde estamos fisicamente</span>
           </div>
         </div>
 
-        <div className='flex justify-between mt-[60px]'>
+        <div className='flex flex-col lg:flex-row gap-10 lg:gap-0 justify-between mt-[60px]'>
           
 
           {locations.map((locations) => (
-          <div className='w-[435px] h-[520px] bg-white rounded-xl' key={locations.id}>
+          <div className='w-full lg:w-[32%] 2xl:w-[435px] 2xl:h-[520px] bg-white rounded-xl' key={locations.id}>
           <div><img src={locations.image} alt='' /> </div>
           <div className='py-[50px] pl-[50px]'>
-            <h3 className='text-[24px] text-[#343434] font-medium mb-[20px]'>{locations.name}</h3> 
-            <p className='text-[18px] text-[#575757] font-light w-[85%] mb-[40px]'>{locations.adress}</p>
-            <a className='flex text-[18px] text-[#0F5197] font-medium items-center gap-[10px] w-max' href={locations.buttonLink}>Ligar <ArrowRight className='mt-[5px]'/></a>
+            <h3 className='text-xl text-[#343434] font-medium mb-[20px]'>{locations.name}</h3> 
+            <p className='text-sm text-[#575757] font-light w-[85%] mb-[40px]'>{locations.adress}</p>
+            <a className='flex text-sm text-[#0F5197] font-medium items-center gap-[10px] w-max' href={locations.buttonLink}>Ligar <ArrowRight className='mt-[5px]'/></a>
           </div>
           </div>
           ))}
@@ -117,15 +117,15 @@ const Contato = () => {
       </div>
     </div>
 
-    <div className='flex items-center h-[1500px] ' id='equipe'>
+    <div className='flex items-center py-36' id='equipe'>
         <div className='container mx-auto'>
-            <div className='flex justify-between'>
-              <div className='w-[40%]'>
-              <span className='flex w-max bg-[#0088FF] py-2 px-5 text-white text-[18px] font-medium rounded-3xl'>CARREIRA</span>
-              <h2 className='text-[42px] text-[#343434] font-medium mb-[30px] mt-[20px]'>Conheça nossa equipe</h2>
-              <p className='text-[18px] text-[#575757] font-light'>A Comabe tem como diferencial a preocupação com a qualidade e a eficiência dos produtos que comercializa, bem como com o atendimento prestado. Sendo assim, a equipe Comabe é especializada e treinada pelas marcas parceiras para oferecer a melhor assistência e atendimento personalizado.</p>
-              <h3 className='text-[24px] text-[#343434] font-medium mt-[40px] mb-[20px]'>O que acha de fazer parte do grupo Comabe?</h3>
-              <p className='text-[18px] text-[#575757] font-light mb-[40px] w-[80%]'>Interessados  preencher os dados do formulário através do botão abaixo</p>
+            <div className='flex flex-col lg:flex-row justify-between'>
+              <div className='w-full lg:w-[40%]'>
+              <span className='flex w-max bg-[#0088FF] py-2 px-5 text-white text-sm font-medium rounded-3xl'>CARREIRA</span>
+              <h2 className='text-3xl text-[#343434] font-medium mb-[30px] mt-[20px]'>Conheça nossa equipe</h2>
+              <p className='text-sm text-[#575757] font-light'>A Comabe tem como diferencial a preocupação com a qualidade e a eficiência dos produtos que comercializa, bem como com o atendimento prestado. Sendo assim, a equipe Comabe é especializada e treinada pelas marcas parceiras para oferecer a melhor assistência e atendimento personalizado.</p>
+              <h3 className='text-xl text-[#343434] font-medium mt-[40px] mb-[20px]'>O que acha de fazer parte do grupo Comabe?</h3>
+              <p className='text-sm text-[#575757] font-light mb-[40px] w-[80%]'>Interessados  preencher os dados do formulário através do botão abaixo</p>
               <ButtonPrimary text='Quero fazer parte' color='ButtonPrimary' linkRef=''/>
               </div>
 
@@ -135,27 +135,27 @@ const Contato = () => {
             </div>
 
             <div className='mt-[150px]'>
-              <h2 className='text-[42px] text-[#343434] font-medium mb-[50px] mt-[20px]'>Nossas divisões</h2>
-              <div className='flex justify-between'>
-                <div className='flex flex-col items-center justify-center w-[320px] h-[275px] shadow-shadow-card rounded-xl'>
+              <h2 className='text-3xl text-[#343434] font-medium mb-[50px] mt-[20px]'>Nossas divisões</h2>
+              <div className='flex flex-wrap gap-2 lg:gap-0 justify-between'>
+                <div className='flex flex-col items-center justify-center w-[48%] lg:w-[24%] 2xl:w-[320px] h-[275px] shadow-shadow-card rounded-xl'>
                     <div><Gestao /></div>
-                    <h2 className='text-[24px] text-[#343434] font-medium mb-[10px] mt-[20px]'>Gestão</h2>
-                    <p className='text-center w-[60%] text-[18px] text-[#575757] font-light'>Conheça mais a nossa história e quem somos</p>
+                    <h2 className='text-xl text-[#343434] text-center lg:text-left font-medium mb-[10px] mt-[20px]'>Gestão</h2>
+                    <p className='text-center w-[60%] text-sm text-[#575757] font-light'>Conheça mais a nossa história e quem somos</p>
                 </div>
-                <div className='flex flex-col items-center justify-center w-[320px] h-[275px] shadow-shadow-card rounded-xl'>
+                <div className='flex flex-col items-center justify-center w-[48%] lg:w-[24%] 2xl:w-[320px] h-[275px] shadow-shadow-card rounded-xl'>
                 <div><Equipe/></div>
-                    <h2 className='text-[24px] text-[#343434] font-medium mb-[10px] mt-[20px]'>Equipe interna</h2>
-                    <p className='text-center w-[80%] text-[18px] text-[#575757] font-light'>Para quem quer tecnologia para sua empresa</p>
+                    <h2 className='text-xl text-[#343434] text-center lg:text-left font-medium mb-[10px] mt-[20px]'>Equipe interna</h2>
+                    <p className='text-center w-[80%] text-sm text-[#575757] font-light'>Para quem quer tecnologia para sua empresa</p>
                 </div>
-                <div className='flex flex-col items-center justify-center w-[320px] h-[275px] shadow-shadow-card rounded-xl'>
+                <div className='flex flex-col items-center justify-center w-[48%] lg:w-[24%] 2xl:w-[320px] h-[275px] shadow-shadow-card rounded-xl'>
                 <div><Assistencia /></div>
-                    <h2 className='text-[24px] text-[#343434] font-medium mb-[10px] mt-[20px]'>Assistência técnica</h2>
-                    <p className='text-center w-[80%] text-[18px] text-[#575757] font-light'>Para quem quer necessita de suporte com equipamentos</p>
+                    <h2 className='text-xl text-[#343434] text-center lg:text-left font-medium mb-[10px] mt-[20px]'>Assistência técnica</h2>
+                    <p className='text-center w-[80%] text-sm text-[#575757] font-light'>Para quem quer necessita de suporte com equipamentos</p>
                 </div>
-                <div className='flex flex-col items-center justify-center w-[320px] h-[275px] shadow-shadow-card rounded-xl'>
+                <div className='flex flex-col items-center justify-center w-[48%] lg:w-[24%] 2xl:w-[320px] h-[275px] shadow-shadow-card rounded-xl'>
                 <div><Consultor /></div>
-                    <h2 className='text-[24px] text-[#343434] font-medium mb-[10px] mt-[20px]'>Consultores externos</h2>
-                    <p className='text-center w-[80%] text-[18px] text-[#575757] font-light'>Fale conosco para saber qual o consultor mais próximo de você</p>
+                    <h2 className='text-xl text-[#343434] text-center lg:text-left font-medium mb-[10px] mt-[20px]'>Consultores externos</h2>
+                    <p className='text-center w-[80%] text-sm text-[#575757] font-light'>Fale conosco para saber qual o consultor mais próximo de você</p>
                 </div>
               </div>
             </div>
--- src/pages/QuemSomos.js
+++ src/pages/QuemSomos.js
@@ -28,9 +28,9 @@ function QuemSomos() {
     <div className="bg-hero-bg-quem-somos w-full h-[500px] sm:h-[490px] md:h-[490px]  lg:h-[490px] bg-cover flex items-center 2xl:h-[720px] relative">
     <div className='container mx-auto'>
       <div>
-        <h2 className='text-white w-max py-3 px-6 rounded-3xl text-[18px] font-medium leading-none mb-[20px] bg-[#0088FF]'>SOBRE NÓS</h2>
-        <h1 className='text-white text-[48px] font-medium leading-none mb-[20px]'>Mantemos a tradição</h1>
-        <p className='text-white text-[18px] font-light w-[90%] md:w-[561px] leading-relaxed'>Desde 1967 inovando em tecnologia e soluções para ambientes de impressão e digitalização de documentos.</p>
+        <h2 className='text-white w-max py-3 px-6 rounded-3xl text-sm font-medium leading-none mb-[20px] bg-[#0088FF]'>SOBRE NÓS</h2>
+        <h1 className='text-white text-4xl font-medium leading-none mb-[20px]'>Mantemos a tradição</h1>
+        <p className='text-white text-sm font-light w-[90%] md:w-[561px] leading-relaxed'>Desde 1967 inovando em tecnologia e soluções para ambientes de impressão e digitalização de documentos.</p>
       </div>
     </div>
     <Mouse className='absolute -bottom-7 left-1/2 -translate-x-1/2' />
@@ -40,24 +40,24 @@ function QuemSomos() {
       <div className='container mx-auto'>
       <div className='flex flex-col lg:flex-row justify-between'>
         <div className='pt-[160px] lg:w-[45%]'>
-          <h2 className='text-[26px] sm:text-[36px] md:text-[36px] lg:text-[28px] xl:text-[36px] 2xl:text-[42px] text-[#343434] font-medium mb-[30px] leading-snug'>Somos especialistas em soluções para impressão e transformação digital de documentos</h2>
-          <p className='text-[15px] sm:text-[16px] md:text-[16px] lg:text-[15px] xl:text-[16px] 2xl:text-[18px] font-light text-[#575757] leading-relaxed mb-[40px]'>Fornecemos soluções completas para sua empresa esquecer de vez de se preocupar com impressoras ou documentos impressos. Facilitamos ao máximo a sua experiência com os nossos serviços, para promover a sua empresa a praticidade necessária para você focar em outras áreas do seu negócio.</p>
+          <h2 className='text-3xl text-[#343434] font-medium mb-[30px] leading-snug'>Somos especialistas em soluções para impressão e transformação digital de documentos</h2>
+          <p className='text-sm font-light text-[#575757] leading-relaxed mb-[40px]'>Fornecemos soluções completas para sua empresa esquecer de vez de se preocupar com impressoras ou documentos impressos. Facilitamos ao máximo a sua experiência com os nossos serviços, para promover a sua empresa a praticidade necessária para você focar em outras áreas do seu negócio.</p>
           <ButtonPrimary linkRef="/Contato#header" text="Agendar Visita" border='border-none' color="ButtonPrimary" />
         </div>
         <div className='bg-[#ECA918] md:w-[100%] md:h-[230px] lg:w-[450px] lg:h-[500px] xl:w-[526px] xl:h-[591px] rounded-2xl sm:rounded-none sm:rounded-t-3xl flex flex-col mt-[20px] pb-[10px] sm:pb-0 pt-[20px] pl-[0px] sm:mt-[30px] md:mt-[50px] lg:mt-[160px] sm:pl-[40px] sm:pt-[30px] md:pt-[50px] lg:pt-[100px]'>
-          <span className='text-white sm:text-[24px] md:text-[26px] lg:text-[20px] xl:text-[22px] 2xl:text-[24px] font-medium ml-[17px] mb-[50px]'>Números que nos orgulhamos:</span>
+          <span className='text-white text-xl font-medium ml-[17px] mb-[50px]'>Números que nos orgulhamos:</span>
           <div className='flex flex-col sm:flex-col md:flex-row gap-3 lg:block'>
             <div className='flex items-center'>
               <img className='w-[60px] md:w-[70px] lg:w-auto' src={MoneyPeople} alt='icone' />
-              <h2 className='text-white font-medium w-full lg:text-[20px] xl:text-[22px] 2xl:text-[24px]'>+ de <span className='lg:text-[24px] xl:text-[28px] 2xl:text-[30px]'>15.000</span> Clientes </h2> 
+              <h2 className='text-white font-medium w-full text-xl'>+ de <span className='text-2xl'>15.000</span> Clientes </h2> 
             </div>
             <div className='flex items-center'>
               <img className='w-[60px] md:w-[70px] lg:w-auto' src={Impressora} alt='icone' />
-              <h2 className='text-white font-medium w-full lg:text-[20px] xl:text-[22px] 2xl:text-[24px]'>+ de <span className='lg:text-[24px] xl:text-[28px] 2xl:text-[30px]'>50.000</span> Equipamentos </h2> 
+              <h2 className='text-white font-medium w-full text-xl'>+ de <span className='text-2xl'>50.000</span> Equipamentos </h2> 
             </div>
             <div className='flex items-center'>
               <img className='w-[60px] md:w-[70px] lg:w-auto' src={Gotas} alt='icone' />
-              <h2 className='text-white font-medium w-full lg:text-[20px] xl:text-[22px] 2xl:text-[24px]'>+ de <span className='lg:text-[24px] xl:text-[28px] 2xl:text-[30px]'>1.000.000</span> Suprimentos </h2> 
+              <h2 className='text-white font-medium w-full text-xl'>+ de <span className='text-2xl'>1.000.000</span> Suprimentos </h2> 
             </div>
           </div>
         </div>
@@ -73,11 +73,11 @@ function QuemSomos() {
         </div>
 
         <div className='md:w-full mt-[50px] md:mt-[50px] lg:mt-0 lg:w-[47%]'>
-          <h2 className='text-[26px] sm:text-[36px] md:text-[36px] lg:text-[28px] xl:text-[36px] 2xl:text-[42px] text-[#343434] font-medium mb-[30px]'>Nosso maior desafio</h2>
-          <p className='text-[15px] sm:text-[16px] md:text-[16px] lg:text-[15px] xl:text-[16px] 2xl:text-[18px] text-[#343434] font-light mb-[40px]'>Fornecer aos nossos clientes as melhores soluções em equipamentos para impressoras, gestão de documentos físicos e digitais, aluguel de equipamentos de impressão, monitoramento e gerenciamento eletrônico de documentos, e migração digital.</p>
+          <h2 className='text-3xl text-[#343434] font-medium mb-[30px]'>Nosso maior desafio</h2>
+          <p className='text-sm text-[#343434] font-light mb-[40px]'>Fornecer aos nossos clientes as melhores soluções em equipamentos para impressoras, gestão de documentos físicos e digitais, aluguel de equipamentos de impressão, monitoramento e gerenciamento eletrônico de documentos, e migração digital.</p>
 
-          <span className='text-[18px] md:text-[20px] lg:text-[20px] xl:text-[22px] 2xl:text-[24px] text-[#343434] font-medium flex items-center gap-[15px]'><Check className='w-[22px] md:w-[26px] xl:w-[26px] 2xl:w-auto' /> As melhores soluções pra você</span>
-          <p className='text-[15px] sm:text-[16px] md:text-[16px] lg:text-[15px] xl:text-[16px] 2xl:text-[18px] text-[#343434] font-light mt-[30px]'>Somos especialistas em proporcionar a migração digital de documentos dos nossos clientes, soluções completas de gestão de documentos físicos e transformação de materiais impressos em arquivos digitais através do business scan, o processo de digitalização de documentos acessível em poucos cliques.</p>
+          <span className='text-xl text-[#343434] font-medium flex items-center gap-[15px]'><Check className='w-[22px] md:w-[26px] xl:w-[26px] 2xl:w-auto' /> As melhores soluções pra você</span>
+          <p className='text-sm text-[#343434] font-light mt-[30px]'>Somos especialistas em proporcionar a migração digital de documentos dos nossos clientes, soluções completas de gestão de documentos físicos e transformação de materiais impressos em arquivos digitais através do business scan, o processo de digitalização de documentos acessível em poucos cliques.</p>
         </div>
       </div>
       </div>
@@ -87,10 +87,10 @@ function QuemSomos() {
       <div className='container mx-auto'>
         <div className='flex flex-col-reverse md:flex-col-reverse lg:flex-row justify-between'>
           <div className='w-full mt-[50px] md:mt-0 md:w-full lg:w-[50%]'>
-            <h2 className='text-[26px] sm:text-[36px] md:text-[36px] lg:text-[28px] xl:text-[36px] 2xl:text-[42px] text-[#343434] font-medium mb-[30px]'>55 Anos de comprometimento</h2>
-            <p className='text-[15px] sm:text-[16px] md:text-[16px] lg:text-[15px] xl:text-[16px] 2xl:text-[18px] text-[#343434] font-light mb-[30px]'>A Comabe, ao longo dos seus 55 anos de atuação, sempre buscou levar mais tecnologia e inovação para a rotina de seus clientes, trazendo mais praticidade e eficiência na gestão das suas atividades.</p>
-            <p className='text-[15px] sm:text-[16px] md:text-[16px] lg:text-[15px] xl:text-[16px] 2xl:text-[18px] text-[#343434] font-light mb-[30px]'>Todos os nossos serviços buscam entregar para os nossos clientes soluções completas que visam agilizar os seus processos e facilitar o seu dia a dia na gestão de documentos.</p>
-            <p className='text-[15px] sm:text-[16px] md:text-[16px] lg:text-[15px] xl:text-[16px] 2xl:text-[18px] text-[#343434] font-light mb-[40px]'>Contamos com uma equipe qualificada, que está sempre pronta para entregar aos nossos clientes um atendimento personalizado, atendendo empresas das mais variadas áreas e de portes distintos, sempre disponibilizando soluções que visam o aumento de produtividade e eficiência nos processos de impressão.</p>
+            <h2 className='text-3xl text-[#343434] font-medium mb-[30px]'>55 Anos de comprometimento</h2>
+            <p className='text-sm text-[#343434] font-light mb-[30px]'>A Comabe, ao longo dos seus 55 anos de atuação, sempre buscou levar mais tecnologia e inovação para a rotina de seus clientes, trazendo mais praticidade e eficiência na gestão das suas atividades.</p>
+            <p className='text-sm text-[#343434] font-light mb-[30px]'>Todos os nossos serviços buscam entregar para os nossos clientes soluções completas que visam agilizar os seus processos e facilitar o seu dia a dia na gestão de documentos.</p>
+            <p className='text-sm text-[#343434] font-light mb-[40px]'>Contamos com uma equipe qualificada, que está sempre pronta para entregar aos nossos clientes um atendimento personalizado, atendendo empresas das mais variadas áreas e de portes distintos, sempre disponibilizando soluções que visam o aumento de produtividade e eficiência nos processos de impressão.</p>
             <ButtonPrimary text='Agendar Visita' linkRef="/Contato#header" border='border-none' color='ButtonPrimary' />
           </div>
           <div className='relative md:flex justify-center lg:block md:w-full md:h-[600px]  lg:w-[430px] lg:h-[500px] 2xl:w-[501px] 2xl:h-[575px]'>
@@ -99,11 +99,11 @@ function QuemSomos() {
               <div className='flex items-center gap-[20px]'>
                 <img src={ThumbJordano} alt='jordano zanesco'/>
                 <div className='flex flex-col'>
-                <span className='text-[16px] md:text-[18px] font-medium text-[#343434]'>Jordano Zanesco</span>
-                <span className='text-[15px] md:text-[16px] font-light text-[#FF8800]'>CEO da Comabe</span>
+                <span className='text-sm font-medium text-[#343434]'>Jordano Zanesco</span>
+                <span className='text-sm font-light text-[#FF8800]'>CEO da Comabe</span>
                 </div>
               </div>
-              <p className='text-[15px] md:text-[18px] text-[#575757] font-light mt-[25px] w-[90%]'>A Comabe, é uma grande parceira de seus clientes na área de tecnologia</p>
+              <p className='text-sm text-[#575757] font-light mt-[25px] w-[90%]'>A Comabe, é uma grande parceira de seus clientes na área de tecnologia</p>
             </div>
           </div>
         </div>
@@ -118,20 +118,20 @@ function QuemSomos() {
       </div>
     </div>
 
-  <div className='bg-[#F8F8FA] h-[800px] flex items-center'>
+  <div className='bg-[#F8F8FA] py-48 flex items-center mt-20 lg:mt-0'>
     <div className='container mx-auto'>
-        <div className='flex justify-between'>
-          <div className='w-[40%] flex flex-col items-end relative -bottom-[30px]'>
-            <img src={ImageAmbienteMenor} className='absolute -left-[100px] -top-[70px]' alt='imagem do ambiente interno de forma pequena' />
-            <span className='text-[20px] text-[#0F5197] font-medium relative -right-[40px] mb-[30px]'>QUALIDADE E TECNOLOGIA NO SEU NEGÓCIO </span>
+        <div className='flex flex-col lg:flex-row justify-between'>
+          <div className='w-full lg:w-50w 2xl:w-[40%] flex flex-col-reverse lg:flex-col items-end lg:relative -bottom-[30px]'>
+            <img src={ImageAmbienteMenor} className='hidden lg:block absolute left-0 2xl:-left-[100px] -top-[70px]' alt='imagem do ambiente interno de forma pequena' />
+            <span className='text-[20px] text-[#0F5197] lg:w-45w 2xl:w-full font-medium lg:relative  2xl:-right-[40px] mb-[30px]'>QUALIDADE E TECNOLOGIA NO SEU NEGÓCIO </span>
             <img src={ImageAmbiente} alt='imagem do ambiente interno da empresa' />
           </div>
 
-          <div className='w-[40%]'>
-            <h2 className='text-[26px] sm:text-[36px] md:text-[36px] lg:text-[28px] xl:text-[36px] 2xl:text-[42px] font-medium mb-[30px]'>Qualidade e tecnologia no seu negócio</h2>
-            <p className='mb-[30px] text-[18px] font-light'>Conte com as melhores marcas e equipamentos do mercado.</p>
-            <p className='mb-[30px] text-[18px] font-light'>Com a Comabe, você trabalha com as melhores marcas do mercado e ainda tem ganho na produtividade e redução de custos.</p>
-            <p className='mb-[50px] text-[18px] font-light'>Ficou interessado? Então venha conversar com um de nossos consultores e conferir as condições especiais que preparamos para você!</p>
+          <div className='w-full lg:w-[40%]'>
+            <h2 className='text-3xl font-medium mb-[30px]'>Qualidade e tecnologia no seu negócio</h2>
+            <p className='mb-[30px] text-sm font-light'>Conte com as melhores marcas e equipamentos do mercado.</p>
+            <p className='mb-[30px] text-sm font-light'>Com a Comabe, você trabalha com as melhores marcas do mercado e ainda tem ganho na produtividade e redução de custos.</p>
+            <p className='mb-[50px] text-sm font-light'>Ficou interessado? Então venha conversar com um de nossos consultores e conferir as condições especiais que preparamos para você!</p>
             <ButtonPrimary text='Tenho interesse' color='ButtonPrimary' border='border-none' linkRef='/Produtos#produtos' />
           </div>
         </div>
