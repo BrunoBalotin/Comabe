07e4b4fe670e1faaabed0f8177548925898be9ce
Wesley Carvalho <crafael.wesley@gmail.com>
2022-10-03 08:46:54 -0300

    Test page

--- src/components/AnimationRoutes.js
+++ src/components/AnimationRoutes.js
@@ -19,6 +19,8 @@ import { UFS } from '../pages/blog/UFS'
 import { OitoCuidados } from '../pages/blog/OitoCuidados'
 import Digitalizacao2 from '../pages/Digitalizacao2'
 import Digitalizacao3 from '../pages/Digitalizacao3'
+import DigitalizacaoTeste from '../pages/DigitalizacaoTeste'
+import { ObrigadoTeste } from '../pages/ObrigadoTeste'
 
 function AnimationRoutes() {
 
@@ -37,7 +39,9 @@ function AnimationRoutes() {
         <Route path='/digitaliza%C3%A7%C3%A3o'  element={<Digitalizacao/>} />
         <Route path='/digitaliza%C3%A7%C3%A3o$2' exact element={<Digitalizacao2/>} />
         <Route path='/digitaliza%C3%A7%C3%A3o$3' exact element={<Digitalizacao3/>} />
+        <Route path='/digitaliza%C3%A7%C3%A3o-teste' exact element={<DigitalizacaoTeste/>} />
         <Route path='/obrigado' element={<Obrigado/>} />
+        <Route path='/obrigado-teste' element={<ObrigadoTeste/>} />
 
         <Route path='/blog/um-metodo-de-gestao-empresarial-para-ganhar-tempo' element={<MetodoDeGestao/>} />
         <Route path='/blog/paperless-a-cultura-de-eficiencia' element={<Paperless/>} />
