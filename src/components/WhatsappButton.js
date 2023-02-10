91fdbd804b1f51f47f2e9a66f26f58f582fc53aa
Wesley Carvalho <crafael.wesley@gmail.com>
2023-02-10 16:13:36 -0300

    Fix whatsapp number

--- src/components/WhatsappButton.js
+++ src/components/WhatsappButton.js
@@ -1,6 +1,6 @@
 import { BsWhatsapp } from 'react-icons/bs'
 
-export function WhatsappButton({number = "555499780305"}) {
+export function WhatsappButton({number = "555426212330"}) {
     return (
         <a target='_blank' href={`https://api.whatsapp.com/send?phone=${number}`}>
         <div className="fixed bottom-6 right-6 border-green-500 border z-50 w-[60px] h-[60px] rounded-full pulse-anima"></div>
